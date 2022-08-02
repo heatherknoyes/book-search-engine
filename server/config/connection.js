6cabee4b12a7d3bdc87e664841111a5a2e0e6ff7
Heather Noyes <heatherknoyes@gmail.com>
2022-08-02 18:20:35 -0400

    Trying to work on heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://heatherknoyes:OwyTrgTxcQ5IVzvG@googlebooksapp.rsfw78w.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
